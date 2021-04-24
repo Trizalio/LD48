dc7e76bd0c4b889b266413e7975f43478d8405c3
Artheg <artheg@gmail.com>
2021-04-24 15:31:28 +0300

    intro scene added

---
+++ scripts/ui/intro.gd
@@ -0,0 +1,29 @@
+extends Control
+export (Array, Resource) var steps = []
+export (NodePath) var texture_rect_path;
+export (NodePath) var text_label_path;
+export (NodePath) var button_path;
+
+var current_index = 0;
+var texture_rect: TextureRect;
+var text_label: RichTextLabel;
+var button: Button;
+
+func _ready():
+	texture_rect = get_node(texture_rect_path);
+	text_label = get_node(text_label_path);
+	button = get_node(button_path);
+	advance();
+
+func advance():
+	if (current_index == steps.size()):
+		finish();
+		return;
+	var step = steps[current_index];
+	texture_rect.texture = step.texture;
+	text_label.text = step.text;
+	current_index += 1;
+	
+func finish():
+	button.text = 'Play';
+	print('finish')
